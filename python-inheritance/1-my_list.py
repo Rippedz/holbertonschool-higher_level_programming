601d388a589d8c0cf4ffbfabd2758dd5c3ab25d6
Rippedz <ttfoot12@gmail.com>
2024-06-19 09:51:47 +0200

    SQL

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""
-Defines a custom class which is a subclass of the built-in `list` class.
-Method `print_sorted` to print the elements of the list in sorted order.
-"""
+'''class MyList that inherits from list'''
 
 
 class MyList(list):
-    """Class which is a subclass of the built-in list class."""
-
+    '''Class MyList'''
     def print_sorted(self):
-        """
-        Prints the list elements in ascending order. The original
-        list order remains unchanged.
-        """
-        if not all(isinstance(item, int) for item in self):
-            raise TypeError("list elements must be integers")
-        print(sorted(self))
+        new_list = sorted(self)
+        print(new_list)
+        return new_list
